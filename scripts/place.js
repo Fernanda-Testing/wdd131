4c35e009d6f2188cde7ac58b7b92e906fc976dee
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-05-31 02:28:41 -0300

    Added windChill function in js

--- scripts/place.js
+++ scripts/place.js
@@ -6,3 +6,19 @@ const today = new Date();
 currentYear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 lastModified.innerHTML = `<span class="highlight">Last modified: ${document.lastModified}</span>`;
+
+const temperature = document.querySelector("#temperature");
+const windSpeed = document.querySelector("#windSpeed");
+const windChill = document.querySelector("#windChill");
+
+const temp = parseFloat(temperature.textContent);
+const windSp = parseFloat(windSpeed.textContent);
+
+const calculateWindChill = (temperature, windSpeed) => 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+
+if (temp <= 10 && windSp >= 4.8) {
+    const windCh = calculateWindChill(temp, windSp);
+    windChill.innerHTML = `<span class="highlight">${windCh.toFixed(1)}°C</span>`;
+} else {
+    windChill.textContent = "N/A";
+}

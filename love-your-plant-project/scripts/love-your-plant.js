4670ea8cc988a891b7b22e45c7c735b01189ae05
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-06-19 20:23:44 -0300

    references page

--- love-your-plant-project/scripts/love-your-plant.js
+++ love-your-plant-project/scripts/love-your-plant.js
@@ -1,5 +1,4 @@
 const currentYear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
 
 const hamButton = document.querySelector('#menu');
 const pages = document.querySelector('.pages');
@@ -7,8 +6,6 @@ const pages = document.querySelector('.pages');
 const today = new Date();
 
 currentYear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
-
-lastModified.innerHTML = `<span class="highlight">Last modified: ${document.lastModified}</span>`;
 
 hamButton.addEventListener('click', () => {
     pages.classList.toggle('open');
